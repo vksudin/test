cb71dd1c6c12988c19c41f42c8c808235143807d
Sudin Jana <sudinjana12@gmail.com>
2023-01-04 12:43:13 +0530

    Hello java is changed

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int x=1;
+        if(x==1)
+        {
+            System.out.println("Value of x is 1");
+        }
+        else if(x==2)
+        {
+            System.out.println("value of x is 2");
+        }
     }
 }
